b3d74af364ea252ee8aded89c62471029d0ce5c4
= <olavbb@hotmail.com>
2014-12-18 16:06:02 +0100

    Added playlists to youtube utility.

--- src/main/java/com/balonbal/slybot/lib/Strings.java
+++ src/main/java/com/balonbal/slybot/lib/Strings.java
@@ -5,5 +5,8 @@ import java.util.regex.Pattern;
 public class Strings {
 
     public static final Pattern youtube = Pattern.compile("(?:https?)://(www\\.|)youtu(\\.be|be\\.com)");
+    public static final String YOUTUBE_BASE = "https://gdata.youtube.com/feeds/api/";
+    public static final String YOUTUBE_VIDEO = YOUTUBE_BASE + "videos/";
+    public static final String YOUTUBE_PLAYLIST = YOUTUBE_BASE + "playlists/";
 
 }
