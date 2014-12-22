bb35e0e2e3d0e78c5254469350a33b9a38a5f395
= <olavbb@hotmail.com>
2014-12-22 20:31:06 +0100

    Added playlists to youtube utility.

--- src/main/java/com/balonbal/slybot/util/Youtube.java
+++ src/main/java/com/balonbal/slybot/util/Youtube.java
@@ -1,16 +1,20 @@
 package com.balonbal.slybot.util;
 
 import com.balonbal.slybot.SlyBot;
+import com.balonbal.slybot.lib.Strings;
 import org.pircbotx.Colors;
 import org.pircbotx.hooks.Event;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Random;
@@ -19,8 +23,9 @@ import java.util.regex.Pattern;
 
 public class Youtube {
 
-    File f;
     private static final Pattern videoCode = Pattern.compile("(?<=(v=|/))(?<!user/)[\\w\\d\\-_]{11}");
+    private static final Pattern playlistCode = Pattern.compile("(?<=(list=))[\\w\\d\\-_]+");
+    File f;
 
 	public Youtube() {
 
@@ -29,54 +34,84 @@ public class Youtube {
     public void parseVideoInfo(String url, Event<SlyBot> event) throws IllegalArgumentException {
 	    //Remove any extra parameters
         Matcher matcher = videoCode.matcher(url);
+        Matcher playlistMatcher = playlistCode.matcher(url);
+        String playlist = null;
+
+        if (playlistMatcher.find()) {
+            playlist = playlistMatcher.group();
+        }
+
         if (!matcher.find()) throw new IllegalArgumentException("Argument did not contain a valid video code");
         url = url.substring(matcher.start(), matcher.end());
 
 		Random r = new Random();
-        long then = System.currentTimeMillis();
-        System.out.println("Fetching file from https://gdata.youtube.com/feeds/api/videos/" + url);
 
         //Create a new temp file
 		f = new File("tmp_" + r.nextInt(10000));
-		try {
+        File playlistFile = new File("tmp_" + r.nextInt(10000));
+        try {
 			f.createNewFile();
-		} catch (IOException e1) {
+            if (playlist != null) playlistFile.createNewFile();
+        } catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		try {
-			//Fetch the file from the link
-			URL u = new URL("https://gdata.youtube.com/feeds/api/videos/" + url);
-			InputStreamReader is = new InputStreamReader(u.openStream());
-			BufferedReader dis = new BufferedReader(is);
-			
-			String s;
-			
-			//Write to file
-			FileWriter filestream = new FileWriter(f);
-			BufferedWriter out = new BufferedWriter(filestream);
-			
-			while ((s = dis.readLine()) != null)  {
-				out.write(s);
-			}
-			
-			//Close the outputbufferer
-			out.close();
-		} catch (IOException e) {
-            System.out.println("An error occured while reading file: " + e.toString());
+
+        try {
+            downloadToFile(new URL(Strings.YOUTUBE_VIDEO + url), f);
+            if (playlist != null) downloadToFile(new URL(Strings.YOUTUBE_PLAYLIST + playlist), playlistFile);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         }
-        long now = System.currentTimeMillis();
-        System.out.println("Fetched file in " + (now - then) + "ms");
 
         HashMap<String, String> map = parseXmlFile(f);
         map.put("id", url);
+        if (playlist != null) {
+            map.put("playlist_id", playlist);
+            appendPlaylistInfo(map, playlistFile);
+            playlistFile.delete();
+        }
 
-        printInfo(event, map);
+        printInfo(event, map, playlist != null);
 
         f.delete();
 	}
-	
-	public void printInfo(Event<SlyBot> event, HashMap<String, String> items) {
+
+    private void downloadToFile(URL url, File file) {
+
+        long then = System.currentTimeMillis();
+        System.out.println("Fetching file from: " + url.getHost() + url.getPath());
+
+        try {
+            //Open the remote file for reading
+            InputStreamReader inputStreamReader = new InputStreamReader(url.openStream());
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+
+            //Open the local file for writing
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+
+            //Write to file
+            String s;
+            while ((s = reader.readLine()) != null) {
+                writer.write(s);
+            }
+
+            //Clean up
+            writer.close();
+            fileWriter.close();
+            reader.close();
+            inputStreamReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Log the time taken
+        long now = System.currentTimeMillis();
+        System.out.println("Fetched file in " + (now - then) + "ms");
+    }
+
+    public void printInfo(Event<SlyBot> event, HashMap<String, String> items, boolean playlist) {
 
         String id = items.get("id");
 		String creator = items.get("creator");
@@ -85,10 +120,10 @@ public class Youtube {
 		String duration = items.get("duration");
 		
 		//Format the view counter to separate each thousand
-		String viewCount = String.format("%,d", Integer.parseInt(views));
-		
-		
-		int seconds = Integer.parseInt(duration);
+        String viewCount = String.format("%,d", Long.parseLong(views));
+
+
+        int seconds = Integer.parseInt(duration);
 		duration = "";
 		//If the time exceeds one hour
 		if (seconds > 3600) {
@@ -98,11 +133,41 @@ public class Youtube {
 		duration += ((seconds / 60 > 9) ? (seconds / 60) : "0" + (seconds/60)) + ":" + ((seconds % 60 > 9) ? (seconds % 60) : "0" + (seconds % 60));
 		
 		//Send the results for the video back to the channel/user
-		event.getBot().reply(event, "[http://youtu.be/" + Colors.PURPLE + id + Colors.NORMAL + "] - " + Colors.BOLD + Colors.BLUE + creator + Colors.NORMAL + ": " + Colors.DARK_GREEN + title + Colors.NORMAL + " [ " + Colors.BOLD + duration + Colors.NORMAL + " ] [ v: " + Colors.BOLD + viewCount + Colors.NORMAL + " ]");
-		
-	}
-	
-	private HashMap<String, String> parseXmlFile(File f){
+        event.getBot().reply(event, "[http://youtu.be/" + Colors.PURPLE + id + Colors.NORMAL + "] - " +
+                Colors.BOLD + Colors.BLUE + creator + Colors.NORMAL + ": " + Colors.DARK_GREEN + title + Colors.NORMAL +
+                " [ " + Colors.BOLD + duration + Colors.NORMAL + " ]" +
+                " [ v: " + Colors.BOLD + viewCount + Colors.NORMAL + " ]");
+
+        if (playlist) {
+            event.getBot().reply(event, "[Playlist] - " +
+                    Colors.BOLD + Colors.BLUE + items.get("playlist_owner") + Colors.NORMAL + ": " + Colors.DARK_GREEN + items.get("playlist_title") + Colors.NORMAL +
+                    " [ size: " + Colors.BOLD + items.get("playlist_size") + Colors.NORMAL + " ]");
+        }
+    }
+
+    private void appendPlaylistInfo(HashMap<String, String> map, File file) {
+        try {
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
+            Document document = builder.parse(file);
+
+            document.getDocumentElement().normalize();
+
+            Element list = (Element) document.getElementsByTagName("feed").item(0);
+
+            map.put("playlist_owner", list.getElementsByTagName("author").item(0).getChildNodes().item(0).getChildNodes().item(0).getNodeValue());
+            map.put("playlist_title", list.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue());
+            map.put("playlist_size", list.getElementsByTagName("openSearch:totalResults").item(0).getChildNodes().item(0).getNodeValue());
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private HashMap<String, String> parseXmlFile(File f){
 		
 		//Store the found values in a hashmap
 		
