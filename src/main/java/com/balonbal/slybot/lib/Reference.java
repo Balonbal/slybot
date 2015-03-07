27369156e1cf91283567d5741e1f19139bddeedf
Balonbal <olavbb@hotmail.com>
2015-03-07 16:56:15 +0100

    Added MAL credentials verifier.

--- src/main/java/com/balonbal/slybot/lib/Reference.java
+++ src/main/java/com/balonbal/slybot/lib/Reference.java
@@ -15,6 +15,26 @@ public class Reference {
     public static final int REQUIRES_OP_BOT = 2;
     public static final int REQUIRES_OP_BOTH = 3;
 
+    /* MAL API CONSTANTS */
+
+    //URLs
+    public static final String MAL_VERIFY_CREDENTIALS = "http://myanimelist.net/api/account/verify_credentials.xml";
+    public static final String MAL_ANIME_SEARCH_BASE = "http://myanimelist.net/api/anime/search.xml?q=";
+
+    //XML tags
+    public static final String MAL_ANIME_ID = "id";
+    public static final String MAL_ANIME_NAME = "name";
+    public static final String MAL_ANIME_ENGLISH_NAME = "english";
+    public static final String MAL_ANIME_SYNONYMS = "synonyms";
+    public static final String MAL_ANIME_NUM_EPISODES = "episodes";
+    public static final String MAL_ANIME_TYPE = "type";
+    public static final String MAL_ANIME_STATUS = "status";
+    public static final String MAL_ANIME_START_DATE = "start_date";
+    public static final String MAL_ANIME_END_DATE = "end_date";
+    public static final String MAL_ANIME_SYNOPSIS = "synopsis";
+    public static final String MAL_ANIME_IMAGE = "image";
+
+
     /* CONFIGURATION CONSTANTS */
     public static final String CONFIG_BASE_DIR = "config/";
     public static final String CONFIG_CHANNEL_FILE = CONFIG_BASE_DIR + "$NETWORK/$CHANNEL.yaml";
